f7cdabdbe21c0cd15b5cd44157817097918645db
gutoFaria <augustofaria1900@gmail.com>
2021-07-20 17:34:06 -0300

    primeiro

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Products from "./Products";
+
+function App() {
+  return (
+    <div>
+        <h1>
+          Learn React Hooks
+          <Products/>
+        </h1>
+    </div>
+  );
+}
+
+export default App;
