ee5fe0b97baa0038e5801bc86d7a6498d82c9bc4
gutoFaria <augustofaria1900@gmail.com>
2021-08-05 13:29:45 -0300

    babel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Rating from './components/Rating';
+//import Products from './components/Products';
+//import {Button} from 'react-bootstrap';
+
 function App() {
+  const isValid = true;
   return (
     <div>
-         
+      {/*
+        <Products/>
+         <Button variant="primary" disabled={!isValid}>Default</Button>
+      */}
+         <Rating rating="1"/>
+         <Rating rating="2"/>
+         <Rating rating="3"/>
+         <Rating rating="3"/>
+         <Rating rating="4"/>
+         <Rating rating="5"/>
     </div>
   );
 }
---
+++ src/components/Rating.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rating(props){
+    return(
+        <div>
+            <h1>Rating : {props.rating}</h1>
+        </div>
+    )
+}
+
+export default Rating;
